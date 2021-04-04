c9e4b1be9ac7ca126488e66a9dcf501cfa964896
angel <angelgg0700@gmail.com>
2021-04-04 05:22:55 -0700

    Main sencillo

--- src/main/java/proyecto/jpa/Main.java
+++ src/main/java/proyecto/jpa/Main.java
@@ -6,12 +6,12 @@ import javax.persistence.*;
 public class Main {
 
 	public static void main(String[] args) {
-		invocaEM();
-	}
-	
-	public static void invocaEM() {
-		EntityManager EM = JPAUtil.getEntityManager();
-		System.out.println("Creación de tablas realizada correctamente");
-	}
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("proyecto.jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.close();
+		emf.close();
+				
+	}	
 
 }
