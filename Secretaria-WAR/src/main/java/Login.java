c69483ac19c076db071517cff00dfb9d73d6e560
AlejandroArcoPu <alejandroarpu@gmail.com>
2021-05-16 02:06:56 -0700

    creadas varias paginas xhtml

---
+++ Secretaria-WAR/src/main/java/Login.java
@@ -0,0 +1,42 @@
+
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+@Named(value = "login")
+@RequestScoped
+public class Login {
+	
+	private String nombre,contrasenia;
+	
+	private UIComponent mybutton;
+
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+    public String entrar() {
+    
+        if(nombre.equals("admin") && contrasenia.equals("admin")) return "inicio.xhtml";
+        else  
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Username or password is incorrect"));
+        return null;
+    }
+
+}
